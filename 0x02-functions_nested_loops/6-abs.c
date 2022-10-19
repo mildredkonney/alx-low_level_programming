d1f0569307e15f5ccb6b3c1c88a1245b1184c5d7
mildredkonney <mildredkonney9@gmail.com>
2022-10-18 19:23:43 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+/**
+ * _abs - Computes the absolute vale of an integer
+ * @t: The integer to be printed
+ * Return: The absolute value
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
