a418bcd870ceb1a4a1be93d04de5befa0bba3bdb
mildredkonney <mildredkonney9@gmail.com>
2022-10-21 08:54:46 -0700

    task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -20,7 +20,7 @@ void print_remaining_days(int month, int day, int year)
 			day++;
 		}
 
-		printf("Days of the year: %d\n", day);
+		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
